0e889f64e5f5b4ae31a05d49739aa4f0617e9b7c
Setare <setazarm@gmail.com>
2023-07-24 09:21:14 +0000

    initialize store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import items from "../data/item.json"
+import{Col,Row} from "react-bootstrap"
 
 const Store = () => {
   return (
+    <>
     <div>Store</div>
+    <Row md={2} xs={1} lg={3} className="g-3">
+      {items.map(item=>(
+        <Col key={item.id}><StoreItem {...item}/></Col>
+      ))}
+    </Row>
+    </>
+
   )
 }
 
