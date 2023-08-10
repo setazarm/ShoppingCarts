5399e2e052901f4be7a283006df73fd7292411b8
Setare <setazarm@gmail.com>
2023-08-10 10:30:28 +0000

    add a function for total price in shopping card

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,7 @@
 import items from "../data/item.json";
+import { Button, Stack } from "react-bootstrap";
+import {formatCurrency} from "../utilities/formatCurrency"
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 type CartItemProps = {
   id: number;
@@ -6,16 +9,33 @@ type CartItemProps = {
 };
 
 const CartItem = ({ id, quantity }: CartItemProps) => {
+  const{removeItem}=useShoppingCart()
   const item = items.find((i) => i.id === id);
-  
+
   return (
-    <div>
-       
-       <img
+    <Stack directon="horizental" gap={2} className="d-flex align-items-center">
+      <img
         src={item.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
       />
-    </div>
+      <div>
+      <div>
+        {item.name}{" "} 
+        {quantity > 1 && (<span className="text-muted" style={{ fontSize: ".65rem" }}>X {quantity}</span>)}
+      </div>
+      <div className="text-muted" style={{ fontSize: ".75rem" }}>
+        {formatCurrency(item.price)}
+      </div>
+      </div>
+      <div>
+      <div>
+        {formatCurrency(item.price *quantity)}
+      </div>
+      <Button onClick={()=>removeItem(item.id)}>&times;</Button>
+      </div>
+
+
+    </Stack>
   );
 };
 
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,17 +1,25 @@
-import { useShoppingCart } from "../context/ShoppingCartContext"
-import CartItem from "./CartItem"
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import CartItem from "./CartItem";
+import items from "../data/item.json"
+import { formatCurrency } from "../utilities/formatCurrency";
 
 const ShoppingCart = () => {
-    const{cartItem}=useShoppingCart()
+  const { cartItem } = useShoppingCart();
 
   return (
-    <div>Shopping Cart
-        {cartItem?.map(item=>{
-         return <CartItem key={item.id} {...item}/>
-        }
-        )}
+    <div>
+      Shopping Cart
+      {cartItem?.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <div>total:
+        {formatCurrency(cartItem.reduce((a,c)=>{
+          let item= items.find(i =>i.id===c.id)
+          return (item.price * c.quantity) + a
+        },0))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
